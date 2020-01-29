a4713e7cba49368649e33ae53248a51d9221a5d8
Stegura Alex <stegura.alex@gmail.com>
2020-01-30 01:53:19 +0300

    done

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/fosf5f0nv83r9ljn/branch/master?svg=true)](https://ci.appveyor.com/project/Stegur/ahj-1-2-move-el/branch/master)
---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+import getRandom from './utils';
+
+// DOM Elements
+const $cells = [...document.querySelectorAll('.field-cell')];
+
+// New elements
+const img = document.createElement('img');
+img.src = '/images/goblin.png';
+img.alt = 'satan';
+
+// Generate random start cell
+const randStartCell = getRandom(0, $cells.length - 1);
+$cells[randStartCell].appendChild(img);
+
+// Generate random cell in interval
+setInterval(() => {
+  const randCell = getRandom(0, $cells.length - 1);
+  if (randCell !== randStartCell) {
+    $cells[randCell].appendChild(img);
+  }
+}, 3000);
